6af36b5f61e59f12f6e76b4df840c709f1ff866e
Meghrig Terzian <meghrig.terzian@johnabbott.qc.ca>
2021-01-08 22:45:12 -0500

    solution.cpp added

---
+++ solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        if (arr.size()==1) return true;
+        int i=0;
+        while(arr[i]<arr[i+1])
+        {
+            i++;
+        }
+        if(i==0)  return false;
+        int peak=i;
+        while(i<arr.size()-1)
+        {
+            if(arr[i]>arr[i+1])
+                i++;
+            else
+                return false;
+        }
+        if(peak==i) return false;
+        
+        return false;
+    }
+};
